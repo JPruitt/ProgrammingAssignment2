4a69f3795df21db3fe1ee208aa1b4bf24b5647b2
Joe Pruitt <rgrjoey001@hotmail.com>
2014-10-22 10:48:18 -0500

    Programming Solution
    Programming solution with test data.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,116 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment #2
+## Pruitt
 
-## Write a short comment describing this function
-
+## makeCacheMatrix()
+## Creates a special "matrix" object that can cache its inverse.
+## Modified from makeVector()
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(cacheSolve) m <<- cacheSolve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve()
+## Computes the inverse of the special "matrix" returned by makeCacheMatrix(). 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
+## Modified from cacheMean()
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data) %*% data
+        x$setinv(m)
+        m
 }
+
+## Test Data from Discussion Boards
+## myMatrix<-rbind(c(1,2), c(3,4))
+## invMatrix<-solve(myMatrix) %*% myMatrix
+
+## a <- makeCacheMatrix(myMatrix)
+
+## a$get()
+##       [,1] [,2]
+## [1,]    1    2
+## [2,]    3    4
+
+## a$getinv()
+## NULL
+
+## cacheSolve(a)
+##       [,1]         [,2]
+## [1,]    1 4.440892e-16
+## [2,]    0 1.000000e+00
+
+## a$getinv()  
+##       [,1]         [,2]
+## [1,]    1 4.440892e-16
+## [2,]    0 1.000000e+00
+
+## cacheSolve(a)
+## getting cached data
+##       [,1]         [,2]
+## [1,]    1 4.440892e-16
+## [2,]    0 1.000000e+00
+
+## a$set(invMatrix)
+## a$getinv()
+## NULL
+
+## cacheSolve(a)
+##       [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
+
+## cacheSolve(a)
+## getting cached data
+##       [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
+
+## a$get()
+##       [,1]         [,2]
+## [1,]    1 4.440892e-16
+## [2,]    0 1.000000e+00
+
+## a$setinv(0)  
+## a$getinv()
+## [1] 0
+
+## a$get()
+##       [,1]         [,2]
+## [1,]    1 4.440892e-16
+## [2,]    0 1.000000e+00
+
+## cacheSolve(a)
+## getting cached data
+## [1] 0
+
+## a <- makeCacheMatrix(myMatrix)
+## a$get()
+##       [,1] [,2]
+## [1,]    1    2
+## [2,]    3    4
+
+## cacheSolve(a)
+##       [,1]         [,2]
+## [1,]    1 4.440892e-16
+## [2,]    0 1.000000e+00
+
+## cacheSolve(a)
+## getting cached data
+##       [,1]         [,2]
+## [1,]    1 4.440892e-16
+## [2,]    0 1.000000e+00
